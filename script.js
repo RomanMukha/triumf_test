17532177f7d563f7d5c07987c8479eacca842d88
Roman Mukha <rm@e-cashier.ru>
2021-03-26 11:36:51 +0300

    адаптивность, небольшие изменения в скрипте

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const typeInput = document.getElementById("typeInput")
 const colorInput = document.getElementById("colorInput")
 const tableBody = document.getElementById("tableBody")
 
-const refreshTable = function() {
-    tableBody.innerHTML = ''
-    data.forEach((item, index) => {
-        const tableRow = document.createElement("tr")
-        tableRow.id = index
-        tableRow.innerHTML = `
+const refreshTable = () => {
+  tableBody.innerHTML = ''
+  data.forEach((item, index) => {
+    const tableRow = document.createElement("tr")
+    tableRow.id = index
+    tableRow.innerHTML = `
         <td class="cell">
             <div class="color-circle" style="background-color: ${item.color}"></div>
         </td>
@@ -18,60 +18,60 @@ const refreshTable = function() {
         <td class="cell control-cell">
             <button id="upBtn_${index}" class="cell-btn">↑</button>
             <button id="downBtn_${index}" class="cell-btn">↓</button>
-            <button id="deleteBtn_${index}" data-id="${index}" class="cell-btn">❌</button>
+            <button id="deleteBtn_${index}" class="cell-btn">❌</button>
         </td>
     `
-        tableBody.append(tableRow)
-
-        const deleteBtn = document.getElementById("deleteBtn_" + index)
-        deleteBtn.addEventListener("click", () => {
-            data.splice(deleteBtn.dataset.id, 1)
-            refreshTable()
-        })
-        const upBtn = document.getElementById("upBtn_" + index)
-        upBtn.addEventListener("click", () => {
-            const currentEl = data[index]
-            const upperEl = data[index - 1]
-            if (upperEl) {
-                data[index] = upperEl
-                data[index - 1] = currentEl
-            }
-            refreshTable()
-        })
-        const downBtn = document.getElementById("downBtn_" + index)
-        downBtn.addEventListener("click", () => {
-            const currentEl = data[index]
-            const downEl = data[index + 1]
-            if (downEl) {
-                data[index] = downEl
-                data[index + 1] = currentEl
-            }
-            refreshTable()
-        })
+    tableBody.append(tableRow)
+    
+    const deleteBtn = document.getElementById("deleteBtn_" + index)
+    deleteBtn.addEventListener("click", () => {
+      data.splice(index, 1)
+      refreshTable()
     })
-    localStorage.setItem('tableData', JSON.stringify(data))
+    const upBtn = document.getElementById("upBtn_" + index)
+    upBtn.addEventListener("click", () => {
+      const currentEl = data[index]
+      const upperEl = data[index - 1]
+      if (upperEl) {
+        data[index] = upperEl
+        data[index - 1] = currentEl
+      }
+      refreshTable()
+    })
+    const downBtn = document.getElementById("downBtn_" + index)
+    downBtn.addEventListener("click", () => {
+      const currentEl = data[index]
+      const downEl = data[index + 1]
+      if (downEl) {
+        data[index] = downEl
+        data[index + 1] = currentEl
+      }
+      refreshTable()
+    })
+  })
+  localStorage.setItem('tableData', JSON.stringify(data))
 }
 
 if (localStorage.getItem('tableData')) {
-    data = JSON.parse(localStorage.getItem('tableData'))
-    refreshTable()
+  data = JSON.parse(localStorage.getItem('tableData'))
+  refreshTable()
 } else {
-    data = []
+  data = []
 }
 
 addButton.addEventListener("click", () => {
-    if (nameInput.value.length === 0) {
-        alert('Введите имя!')
-    } else if (typeInput.value.length === 0) {
-        alert('Введите тип!')
-    } else {
-        const newObj = {
-            name: nameInput.value,
-            type: typeInput.value,
-            color: colorInput.value,
-        }  
-        data.push(newObj)
-        refreshTable()
+  if (nameInput.value.length === 0) {
+    alert('Введите имя!')
+  } else if (typeInput.value.length === 0) {
+    alert('Введите тип!')
+  } else {
+    const newObj = {
+      name: nameInput.value,
+      type: typeInput.value,
+      color: colorInput.value,
     }
+    data.push(newObj)
+    refreshTable()
+  }
 })  
 
